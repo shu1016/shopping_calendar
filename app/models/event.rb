dfbb172d76a09439c6a9319d4dfa01bb93aceda2
shu1016 <s.a.m-kawai@outlook.jp>
2024-06-26 22:25:22 +0900

    時間のバリテーション追記

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,10 @@ class Event < ApplicationRecord
   validates :content, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
+
+  validate :start_end_check
+
+  def start_end_check
+    errors.add(:end_time, "は開始時間より遅い時間を設定してください") if self.start_time > self.end_time
+  end
 end
