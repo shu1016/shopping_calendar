bc96ae865a4d82746ae607b09b0d796dc7a20057
shu1016 <s.a.m-kawai@outlook.jp>
2024-07-03 12:10:25 +0900

    モーダル表示調整

--- app/javascript/modal.js
+++ app/javascript/modal.js
@@ -7,16 +7,13 @@ document.addEventListener("turbo:load", function() {
     modalButton.addEventListener('click', (e) => {
       e.preventDefault()
       modals[index].classList.add('is-open');
-      let x = e.clientX;
+      const x = e.clientX;
+      console.log (e.clientX)
 
-      // manipulate the modal position
-      // Here, you can add your own logic to decide the modal position, for now, we will use the simplest one, decide based on the half of the width
-      if (x > window.innerWidth / 2){
-        modals[index].style.left = 'auto';
-        modals[index].style.right = '0px';
+      if (x > window.innerWidth / 2.6){
+        modals[index].style.right = '336px';
       }else{
-        modals[index].style.left = '0px';
-        modals[index].style.right = 'auto';
+        modals[index].style.left = '10px';
       }
   
     });
