a16c1866ce6edd9e7c1f75a9a4bba5b871096631
shu1016 <s.a.m-kawai@outlook.jp>
2024-07-08 11:03:41 +0900

    toppage導入

---
+++ app/javascript/top_modal.js
@@ -0,0 +1,18 @@
+document.addEventListener("turbo:load", function() {
+  const topModals = document.querySelectorAll('.top-js-modal');
+  const topModalButtons = document.querySelectorAll('.top-js-modal-button');
+  const topModalCloses = document.querySelectorAll('.top-js-close-button');
+
+  topModalButtons.forEach((topModalButton, index) => {
+    topModalButton.addEventListener('click', (e) => {
+      e.preventDefault()
+      topModals[index].classList.add('top_is-open');
+    });
+  });
+
+  topModalCloses.forEach((topModalClose, index) => {
+    topModalClose.addEventListener('click', () => {
+      topModals[index].classList.remove('top_is-open');
+    });
+  });
+});
