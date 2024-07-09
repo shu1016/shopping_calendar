f0dd76cadc1fd8671cc0096391081c99ff8d78b8
shu1016 <s.a.m-kawai@outlook.jp>
2024-07-09 16:03:18 +0900

    タッチスタート追加

--- app/javascript/top_modal.js
+++ app/javascript/top_modal.js
@@ -8,10 +8,17 @@ document.addEventListener("turbo:load", function() {
       e.preventDefault()
       topModals[index].classList.add('top_is-open');
     });
+    topModalButton.addEventListener('touchstart', (e) => {
+      e.preventDefault();
+      topModals[index].classList.add('top_is-open');
+    });
   });
 
   topModalCloses.forEach((topModalClose, index) => {
     topModalClose.addEventListener('click', () => {
+      topModals[index].classList.remove('top_is-open');
+    });
+    topModalClose.addEventListener('touchstart', () => {
       topModals[index].classList.remove('top_is-open');
     });
   });
