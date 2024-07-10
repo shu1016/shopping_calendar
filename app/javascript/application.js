53d71573538e74a63d4799f9e88d590034a95e7f
shu1016 <s.a.m-kawai@outlook.jp>
2024-07-10 22:35:55 +0900

    ログアウト確認3

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//import "memo_list"
-//import "modal"
-//import "top_modal"
