ea5a21f82e1d3eec9fbe9bf2b52696eeaa1654e2
shu1016 <s.a.m-kawai@outlook.jp>
2024-07-12 12:43:49 +0900

    ログアウト確認２

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users
   root to: 'toppages#index'
   resources :users, only: [:index, :show]
   resources :events do
@@ -7,8 +7,5 @@ Rails.application.routes.draw do
     collection do
       get 'search'
     end
-  end
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 end
