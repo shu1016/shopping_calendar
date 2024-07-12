6e5bbe8f9a7662d5589f99bcf8a0e08154554489
shu1016 <s.a.m-kawai@outlook.jp>
2024-07-12 16:55:07 +0900

    タッチスタート削除

--- app/javascript/top_modal.js
+++ app/javascript/top_modal.js
@@ -8,17 +8,10 @@ const topContents = () => {
       e.preventDefault()
       topModals[index].classList.add('top_is-open');
     });
-    topModalButton.addEventListener('touchstart', (e) => {
-      e.preventDefault();
-      topModals[index].classList.add('top_is-open');
-    });
   });
 
   topModalCloses.forEach((topModalClose, index) => {
     topModalClose.addEventListener('click', () => {
-      topModals[index].classList.remove('top_is-open');
-    });
-    topModalClose.addEventListener('touchstart', () => {
       topModals[index].classList.remove('top_is-open');
     });
   });
